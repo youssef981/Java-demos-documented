9dd5ef40e55890662b4536dd4fbfbf4946be2310
youssef981 <ezzahnouniyoussef538@gmail.com>
2025-10-29 20:02:01 +0100

    adding readme file with details

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import iterator.IteratorInterfaceDemo;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -5,31 +7,6 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
-        List<Integer> values = new ArrayList<>();
-        values.add(1);
-        values.add(2);
-        values.add(3);
-        values.add(4);
-
-        Iterator<Integer> valuesIterator = values.iterator();
-        // make sure you should not use the next() meth more then once in a loop cause you will jump values
-        // remove() meth will pop the last element returned by the iterator.
-
-        while (valuesIterator.hasNext()) {
-            int val = valuesIterator.next();
-            if (val == 3) {
-                valuesIterator.remove();
-            }
-            System.out.println(val);
-        }
-
-        System.out.println("======");
-        for(int val : values){
-            System.out.println(val);
-        }
-
-
-        System.out.println("Hello, Simo");
+        IteratorInterfaceDemo.playIterator();
     }
 }
