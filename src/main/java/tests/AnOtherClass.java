dcfe0b9905c281571d83e24fcbf23d1717f8f6db
youssef981 <ezzahnouniyoussef538@gmail.com>
2025-11-01 09:53:05 +0100

    Nested classes concepts final

--- src/main/java/tests/AnOtherClass.java
+++ src/main/java/tests/AnOtherClass.java
@@ -1,6 +1,12 @@
 package tests;
 
 public class AnOtherClass extends OuterClass.InnerClass{
+
+"""
+Inner classes (non-static nested classes) have an implicit reference to their enclosing outer class instance
+When you extend an inner class, the child class needs that implicit outer class reference
+Since AnOtherClass doesn't automatically have an OuterClass instance, you must provide one explicitly
+"""
     public AnOtherClass() {
         new OuterClass().super();
     }
