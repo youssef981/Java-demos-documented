5607f66ed40c6479b2076d27be8fb5dad20fd16b
youssef981 <ezzahnouniyoussef538@gmail.com>
2025-11-01 09:56:34 +0100

    Nested classes concepts final final

--- src/main/java/tests/AnOtherClass.java
+++ src/main/java/tests/AnOtherClass.java
@@ -8,6 +8,8 @@ When you extend an inner class, the child class needs that implicit outer class 
 Since AnOtherClass doesn't automatically have an OuterClass instance, you must provide one explicitly
 """
     public AnOtherClass() {
+        // as you know, when child class constructor invoked, it first invoked the constructor of the parent.
+        // but here the parent is Inner class, so it can only be accessed by the object f the outerclass only.
         new OuterClass().super();
     }
 
