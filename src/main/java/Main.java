7927f867320e765fc0a315ac299bd44c23cf80cb
youssef981 <ezzahnouniyoussef538@gmail.com>
2025-11-01 09:07:08 +0100

    Nested classes concepts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 import tests.AnOtherClass;
 import tests.SomeOtherClass;
+import collection.CollectionInterfaceDemo;
+import iterator.IteratorInterfaceDemo;
+import queue_interface.PriorityQueueDemo;
+import test.Employee;
+import test.OuterClass;
+
+import javax.swing.plaf.metal.MetalPopupMenuSeparatorUI;
+import java.lang.classfile.attribute.SourceDebugExtensionAttribute;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Main {
 
@@ -7,6 +18,10 @@ public class Main {
 //        IteratorInterfaceDemo.playIterator();
 //        CollectionInterfaceDemo.playCollection();
 //        PriorityQueueDemo.playPriorityQueueDemo();
+
+        OuterClass outerClass  = new OuterClass();
+        OuterClass.InnerClass2 innerClass2Obj = outerClass.new InnerClass2();
+        innerClass2Obj.display();
 
         SomeOtherClass someOtherClass = new SomeOtherClass();
         someOtherClass.display();
